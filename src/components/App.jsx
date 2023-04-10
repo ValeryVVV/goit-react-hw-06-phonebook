21488e3c6b3e340be90db936e90289776e91d9b6
ValeryVVV <110850242+ValeryVVV@users.noreply.github.com>
2023-04-10 17:02:40 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,67 +5,13 @@ import style from './App.module.css';
 import ContactForm from "./ContactForm/ContactForm";
 
 export default function App() {
-
-    // const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('contacts')) ?? []);
-    // const [filter, setFilter] = useState('');
-
-    // useEffect(() => {
-    //     window.localStorage.setItem('contacts', JSON.stringify(contacts));
-    // }, [contacts]);
-
-    // const addContact = ( name, number ) => {
-    //     const newContact = {
-    //         id: shortid.generate(),
-    //         name: name,
-    //         number: number
-    //     }
-
-    //     const checkedName = contacts.find(contact => 
-    //         contact.name.toLowerCase() === newContact.name.toLowerCase(),
-    //         );
-
-    //         if(checkedName) {
-    //             alert(`${newContact.name} is already in contact.`)
-    //             return;
-    //         }
-
-    //         setContacts([newContact, ...contacts]);
-    // };
-
-    //     const changeFilter = e => {
-    //         setFilter(e.currentTarget.value.trim())
-    //     };
-
-        // const getVisibleContacts = () => {
-        //     const normalizedFilter = filter.toLowerCase();
-        //     return contacts.filter(contact =>
-        //       contact.name.toLowerCase().includes(normalizedFilter)
-        //     );
-        //   };
-        
-        //   const deleteContact = contactId => {
-        //     setContacts(contacts.filter(contact => contact.id !== contactId));
-        //   };
-        
-        //   const visibleContacts = getVisibleContacts();
-        
-
-
     return (
         <div className={style.container}>
             <h1>Phonebook</h1>
             <ContactForm />
             <h2>Contacts</h2>
-            <ContactList />
             <Filter />
-            {/* {contacts.length > 0 ? (
-                <>
-                  
-                </>
-            ): (
-                <h1>Contact list is empty</h1>
-            )}  */}
-
+            <ContactList />
         </div>
     )
 
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,11 +2,11 @@ const { createSlice } = require('@reduxjs/toolkit');
 const shortid = require('shortid');
 
 const contactsInitialState = [
-  { id: 0, text: 'Learn HTML and CSS', completed: true },
-  { id: 1, text: 'Get good at JavaScript', completed: true },
-  { id: 2, text: 'Master React', completed: false },
-  { id: 3, text: 'Discover Redux', completed: false },
-  { id: 4, text: 'Build amazing apps', completed: false },
+  { id: 0, name: 'Learn HTML and CSS', number: true },
+  { id: 1, name: 'Get good at JavaScript', number: true },
+  { id: 2, name: 'Master React', number: false },
+  { id: 3, name: 'Discover Redux', number: false },
+  { id: 4, name: 'Build amazing apps', number: false },
 ];
 
 const contactsSlice = createSlice({
